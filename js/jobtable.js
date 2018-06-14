e35f65636fd05c62a36f06b0b08db8cf22d89ecf
marc2016 <lammers.marc@gmail.com>
2018-06-14 18:32:27 +0200

    Fill job table with jobs.

--- js/jobtable.js
+++ js/jobtable.js
@@ -6,26 +6,32 @@ var _ = require('lodash');
 var moment = require('moment');
 var momentDurationFormatSetup = require("moment-duration-format");
 
-var headers = { "description" : "Aufgabe", "formattedTime": "Dauer", "formattedTimeDeciaml": "Dauer (d)" };
+var headers = { "date": "Datum","description" : "Aufgabe", "projectName":"Projekt","formattedTime": "Dauer", "formattedTimeDeciaml": "Dauer (d)" };
 
 var self = module.exports = {
 
     onLoad: function(){
         var Table = require('table-builder');
-        db.find({}, function (err, docs) {
-            var projectIds = _.map(docs, 'projectId')
-            var jobDocs = docs
-            db_projects.find({ _id: { $in: projectIds }}, function (err, docs) {
-                _.forEach(docs, function(value){
+        db.find({}, function (err, jobDocs) {
+            var projectIds = _.map(jobDocs, 'projectId')
+            db_projects.find({ _id: { $in: projectIds }}, function (err, projectDocs) {
+                
+                _.forEach(jobDocs, function(value){
                     var formatted = moment.duration(value.elapsedSeconds, "seconds").format("hh:mm:ss",{trim: false})
                     value.formattedTime = formatted
                     var decimal = moment.duration(value.elapsedSeconds, "seconds").format("h", 2)
                     value.formattedTimeDeciaml = decimal
+
+                    value.projectName = "-"   
+                    if(value.projectId != undefined) {
+                        var project = _.find(projectDocs, {'_id':value.projectId})
+                        value.projectName = project.name   
+                    }
                 })
     
                 var htmlTable = new Table({'id': 'jobs'})
                 .setHeaders(headers) 
-                .setData(docs)
+                .setData(jobDocs)
                 .render()
                 $('#table').html(htmlTable)
                 $('#jobs').DataTable();
