8890911ce8aa4d3cfcd75fe3550fc79dfb4733d5
marc2016 <lammers.marc@gmail.com>
2019-06-21 20:56:43 +0200

    Menu icons to timerlist added.

--- js/timerlist.js
+++ js/timerlist.js
@@ -146,8 +146,26 @@ class TimerList extends BaseViewModel {
         icon: 'fa fa-plus-circle',
         name: 'Neuer Eintrag',
         method: this.addNewItem.bind(this)
+      },
+      {
+        icon: 'fas fa-umbrella-beach',
+        name: 'Urlaubstag',
+        method: this.addNoneWorkingDayToday.bind(this)
+      },
+      {
+        icon: 'fas fa-user-injured',
+        name: 'Krankheitstag',
+        method: this.addNoneWorkingDay.bind(this)
       }
     ]
+  }
+
+  addNoneWorkingDayToday(){
+    this.addNoneWorkingDay(new moment(),new moment())
+  }
+
+  addNoneWorkingDay(start, end){
+
   }
 
   syncEntry(that,data){
